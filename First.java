25d0d04d680dc18a38e4a38806a37dc164716cb5
ketanwachkar <ketanwachkar@gmail.com>
2025-01-15 00:31:08 +0530

    Rotate Array

---
+++ First.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class First {
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5};
+    int n = arr.length;
+    int temp = arr[0];
+    int k = 2;
+    for (int i = 0; i < k; i++) {
+      for (int j = n - 1; j >= 0; j--) { // Right Rotate the Arryas
+
+        temp = arr[j];
+        arr[j] = arr[0];
+        arr[0] = temp;
+      }
+    }
+    System.out.println(Arrays.toString(arr));
+  }
+}
