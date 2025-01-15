b36dea926beb3b8e9cc0c2dc8301e4447c3d1e12
Ritikapankar <ritikapankar25@gmail.com>
2025-01-15 13:48:39 +0530

    Update First.java

--- First.java
+++ First.java
@@ -5,10 +5,9 @@ public class First {
     int[] arr = { 1, 2, 3, 4, 5};
     int n = arr.length;
     int temp = arr[0];
-    int k = 2;
+    int k = 2; 
     for (int i = 0; i < k; i++) {
-      for (int j = n - 1; j >= 0; j--) { // Right Rotate the Arryas
-
+      for (int j = n - 1; j >= 0; j--) { 
         temp = arr[j];
         arr[j] = arr[0];
         arr[0] = temp;
