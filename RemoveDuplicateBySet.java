3188bbd2047a8870685093f66bb0a2df6495d538
Ritikapankar <ritikapankar25@gmail.com>
2025-01-23 15:07:26 +0530

    Add files via upload

---
+++ RemoveDuplicateBySet.java
@@ -0,0 +1,12 @@
+
+                    public class Array {
+                      public static void main(String[] args) {
+                        ArrayList<Integer> al=new ArrayList<>();
+                        al.add(1);
+                        al.add(2);
+                        al.add(3);
+                        al.add(4);
+                        al.set(0, 5);
+                        System.out.println(al);
+                      }
+                    }
